ecf19d9234c7ffa40f3ee0d685fdf594a068036a
Bindusgowda <84507568+Bindusgowda@users.noreply.github.com>
2024-09-30 11:34:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
-#include "color_pair_test.c"
+#include <stdio.h>
+#include <assert.h>
+#include "colorPair.h"
 
 int main() {
-    // Test cases for converting pair numbers to ColorPair
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
-    // Test cases for converting ColorPair to pair numbers
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
+    printColorCodeReferenceManual(numberOfMajorColors,numberOfMinorColors);
     return 0;
 }
